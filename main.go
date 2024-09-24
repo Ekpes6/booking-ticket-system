a4723d73002cb18196bba8c14d4e8c679cf24d4a
Ekpes6 <felixekpenyong6@gmail.com>
2024-09-24 20:50:10 +0100

    enhancing the booking ticket logic

--- main.go
+++ main.go
@@ -33,30 +33,30 @@ func main() {
 
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
+		//improving on the booking ticket logic
+		if userTickets <= remainingTicket {
+			remainingTicket = remainingTicket - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		if userTickets > remainingTicket {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTicket, userTickets)
-			break
-		}
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remainingTicketing for %v\n", remainingTicket, conferenceName)
 
-		remainingTicket = remainingTicket - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
-
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remainingTicketing for %v\n", remainingTicket, conferenceName)
-
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
-		if remainingTicket == 0 {
-			fmt.Printf("Tickets are sold out")
-			break
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+			if remainingTicket == 0 {
+				fmt.Printf("Tickets are sold out")
+				break
+			} else {
+				fmt.Printf("%v available\n", remainingTicket)
+			}
 		} else {
-			fmt.Printf("%v available\n", remainingTicket)
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTicket, userTickets)
 		}
+
 	}
 
 }
